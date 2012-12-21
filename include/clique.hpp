5ca82dba7aafeeaabc15750a767862364de10fdc
Jack Poulson <jack.poulson@gmail.com>
2012-12-21 11:31:16 -0600

    Updating to Elemental revision 734:1a924b532b19

--- include/clique.hpp
+++ include/clique.hpp
@@ -1,21 +1,10 @@
 /*
-   Clique: a scalable implementation of the multifrontal algorithm
-
    Copyright (C) 2011-2012 Jack Poulson, Lexing Ying, and 
    The University of Texas at Austin
  
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
- 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
- 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   This file is part of Clique and is under the GNU General Public License,
+   which can be found in the LICENSE file in the root directory, or at 
+   <http://www.gnu.org/licenses/>.
 */
 #ifndef CLIQUE_HPP
 #define CLIQUE_HPP 1
